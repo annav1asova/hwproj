13b8d47a877e4f045e5db7eeb10f576b3f9d96fd
NataliaPonomarevaMM <natpon12@gmail.com>
2018-05-20 15:16:05 +0300

    Updated cookie

--- client/src/reducers/auth/auth.epic.js
+++ client/src/reducers/auth/auth.epic.js
@@ -5,11 +5,12 @@ import 'rxjs';
 
 export const startLoginProcessEpic = action$ =>
   action$.ofType('START-LOGIN-PROCESS')
-    .map(action => axios.post('/sign_in_server', {
+    .map(action => {axios.defaults.withCredentials = true;
+    return axios.post('/sign_in_server', {
             'email': action.email,
-            'pass': action.password,
-            withCredentials: true
-        }))
+            'pass': action.password
+        });
+    })
     .map(response => response.isLogged
         ? userLoggedIn(response.isTeacher, response.userCourses, response.fn, response.ln, response.email)
         : userLoggedOut());
